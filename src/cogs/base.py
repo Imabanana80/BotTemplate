45229e4067885efb7675c5d528c132143b29dd52
Imabanana80 <imabanana80.hallo@gmail.com>
2023-10-31 12:04:22 +0800

    added DISCONTINUED readme

--- src/cogs/base.py
+++ src/cogs/base.py
@@ -2,8 +2,6 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-
-
 class Base(commands.Cog):
     def __init__(self, client):
         self.client: discord.Client = client
@@ -11,8 +9,16 @@ class Base(commands.Cog):
     @app_commands.command(name="ping", description="ping the bot")
     async def ping(self, interaction: discord.Interaction):
         await interaction.response.defer()
-        await interaction.followup.send("Pong!")
-    
+        embed = discord.Embed(
+            title="🏓 **Pong!**", 
+            color=discord.Color.blurple(),
+        )
+        embed.add_field(
+            name="**API Latency: **",
+            value=f"```{round(self.client.latency*1000)}ms     ```"
+        )
+        await interaction.followup.send(embed=embed)
+
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'Logged in as {self.client.user}')
@@ -20,7 +26,5 @@ class Base(commands.Cog):
 
     
     
-
-
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(Base(client))
